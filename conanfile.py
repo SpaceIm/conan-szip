42dec20b154644c7a88d9493c01845d0698dce5c
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2020-02-19 18:11:30 +0100

    use patches

--- conanfile.py
+++ conanfile.py
@@ -9,7 +9,7 @@ class SzipConan(ConanFile):
     topics = ("conan", "szip", "compression", "decompression")
     homepage = "https://support.hdfgroup.org/doc_resource/SZIP/"
     url = "https://github.com/conan-io/conan-center-index"
-    exports_sources = "CMakeLists.txt"
+    exports_sources = ["CMakeLists.txt", "patches/**"]
     generators = "cmake"
     settings = "os", "arch", "compiler", "build_type"
     options = {
@@ -46,31 +46,12 @@ class SzipConan(ConanFile):
         os.rename(self.name + "-" + self.version, self._source_subfolder)
 
     def build(self):
-        self._patch_sources()
-        cmake = self._configure_cmake()
-        cmake.build()
-
-    def _patch_sources(self):
+        for patch in self.conan_data["patches"][self.version]:
+            tools.patch(**patch)
         tools.replace_in_file(os.path.join(self._source_subfolder, "CMakeLists.txt"),
                               "set (CMAKE_POSITION_INDEPENDENT_CODE ON)", "")
-        tools.replace_in_file(os.path.join(self._source_subfolder, "src", "CMakeLists.txt"),
-                              "add_library (${SZIP_LIB_TARGET} STATIC ${SZIP_SRCS} ${SZIP_PUBLIC_HEADERS})",
-                              "if (NOT BUILD_SHARED_LIBS)\n" \
-                              "add_library (${SZIP_LIB_TARGET} STATIC ${SZIP_SRCS} ${SZIP_PUBLIC_HEADERS})")
-        tools.replace_in_file(os.path.join(self._source_subfolder, "src", "CMakeLists.txt"),
-                              "if (BUILD_SHARED_LIBS)\n" \
-                              "  add_library (${SZIP_LIBSH_TARGET} SHARED ${SZIP_SRCS} ${SZIP_PUBLIC_HEADERS})",
-                              "else()\n" \
-                              "  add_library (${SZIP_LIBSH_TARGET} SHARED ${SZIP_SRCS} ${SZIP_PUBLIC_HEADERS})")
-        tools.replace_in_file(os.path.join(self._source_subfolder, "src", "CMakeLists.txt"),
-                              "INSTALL_TARGET_PDB (${SZIP_LIB_TARGET} ${SZIP_INSTALL_BIN_DIR} libraries)",
-                              "if (NOT BUILD_SHARED_LIBS)\n" \
-                              "  INSTALL_TARGET_PDB (${SZIP_LIB_TARGET} ${SZIP_INSTALL_BIN_DIR} libraries)\n" \
-                              "endif()")
-        tools.replace_in_file(os.path.join(self._source_subfolder, "src", "szlib.h"),
-                              "#define SZLIB_VERSION \"2.1.1\"",
-                              "#include <stddef.h>\n" \
-                              "#define SZLIB_VERSION \"2.1.1\"")
+        cmake = self._configure_cmake()
+        cmake.build()
 
     def _configure_cmake(self):
         if self._cmake:
